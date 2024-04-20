3d012c1dcf862a29207d66f3566183a8747f9985
Michael de Silva <michael@mwdesilva.com>
2024-04-20 19:56:07 +0530

    WIP fetch Embassy latest FIXME blocking write/read framing errors

--- rtos/src/main.rs
+++ rtos/src/main.rs
@@ -27,14 +27,84 @@ assign_resources! {
     },
 }
 
-bind_interrupts!(struct USART1Irqs {
-    UART8 => usart::InterruptHandler<peripherals::UART8>;
-});
+// NOTE: Only needed for testing with DMA
+// bind_interrupts!(struct USART1Irqs {
+//     UART8 => usart::InterruptHandler<peripherals::UART8>;
+// });
 
 pub fn init() -> (embassy_stm32::Peripherals, cortex_m::Peripherals) {
     info!("Initialising power stage...");
 
-    let config = embassy_stm32::Config::default();
+    let mut config = embassy_stm32::Config::default();
+    {
+        use embassy_stm32::rcc::*;
+        // config.rcc.supply_config = SupplyConfig::LDO;
+        config.rcc.hsi = Some(HSIPrescaler::DIV1); // // 64MHz
+        config.rcc.csi = true;
+        config.rcc.hsi48 = Some(Hsi48Config {
+            sync_from_usb: true,
+        }); // needed for USB
+
+        #[cfg(feature = "stm32h747_400")]
+        {
+            config.rcc.pll1 = Some(Pll {
+                source: PllSource::HSI,
+                prediv: PllPreDiv::DIV4,
+                mul: PllMul::MUL50,
+                divp: Some(PllDiv::DIV2), // ((64/4)*50)/2 = 400MHz
+                divq: Some(PllDiv::DIV8), // ((64/4)*50)/8 = 100MHz / SPI1 cksel defaults to pll1_q
+                divr: None,
+            });
+            config.rcc.pll2 = Some(Pll {
+                source: PllSource::HSI,
+                prediv: PllPreDiv::DIV8,
+                mul: PllMul::MUL50,
+                divp: Some(PllDiv::DIV4), // ((64/8)*50)/4 = 100MHz
+                divq: None,
+                divr: None,
+            });
+        }
+        #[cfg(feature = "stm32h747_480")]
+        {
+            config.rcc.pll1 = Some(Pll {
+                source: PllSource::HSI,
+                prediv: PllPreDiv::DIV8,
+                mul: PllMul::MUL120,
+                divp: Some(PllDiv::DIV2), // ((64/8)*120)/2 = 480MHz
+                divq: Some(PllDiv::DIV8), // ((64/8)*120)/8 = 120MHz / SPI1 cksel defaults to pll1_q
+                divr: None,
+            });
+            config.rcc.pll2 = Some(Pll {
+                source: PllSource::HSI,
+                prediv: PllPreDiv::DIV8,
+                mul: PllMul::MUL50,
+                divp: Some(PllDiv::DIV4), // ((64/8)*50)/4 = 100MHz
+                divq: None,
+                divr: None,
+            });
+        }
+
+        config.rcc.sys = Sysclk::PLL1_P; // 400 Mhz
+        config.rcc.ahb_pre = AHBPrescaler::DIV2; // 200 Mhz
+        config.rcc.apb1_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.apb2_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.apb3_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.apb4_pre = APBPrescaler::DIV2; // 100 Mhz
+        config.rcc.voltage_scale = VoltageScale::Scale0;
+
+        let mut mux = embassy_stm32::rcc::mux::ClockMux::default();
+        mux.adcsel = embassy_stm32::rcc::mux::Adcsel::PLL2_P;
+        config.rcc.mux = mux;
+
+        // RTC
+        config.rcc.ls = LsConfig::default_lse();
+
+        trace!(
+            "rcc.voltage_scale = Voltage::Scale{=i32}",
+            config.rcc.voltage_scale as i32
+        );
+    }
+
     let p: embassy_stm32::Peripherals = embassy_stm32::init(config);
     let core_peri = defmt::unwrap!(cortex_m::Peripherals::take());
 
@@ -64,16 +134,23 @@ pub async fn usart_task(r: USART1Resource) {
 
     let mut config = embassy_stm32::usart::Config::default();
     config.baudrate = 115200;
-    let mut usart = defmt::unwrap!(embassy_stm32::usart::Uart::new(
-        r.peri, r.rx, r.tx, USART1Irqs, NoDma, NoDma, config
+    let mut usart = defmt::unwrap!(embassy_stm32::usart::Uart::new_blocking(
+        r.peri, r.rx, r.tx, config
     ));
 
-    unwrap!(usart.blocking_write(b"Hello Embassy World!\r\n"));
-    info!("wrote Hello, starting echo");
+    for i in 0..2 {
+        unwrap!(usart.blocking_write(b"Hello\r\n"));
+        info!("[{}]: wrote Hello, starting echo", &i);
+    }
 
     let mut buf = [0u8; 1];
     loop {
-        unwrap!(usart.blocking_read(&mut buf));
-        unwrap!(usart.blocking_write(&buf));
+        if let Err(e) = usart.blocking_read(&mut buf) {
+            error!("usart read error: {}", e);
+        }
+
+        if let Err(_e) = usart.blocking_write(&buf) {
+            //
+        }
     }
 }
