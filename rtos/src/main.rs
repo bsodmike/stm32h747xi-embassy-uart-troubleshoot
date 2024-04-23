77b56b8da202e4626315f63612f22120908cd11c
Michael de Silva <michael@mwdesilva.com>
2024-04-23 07:20:46 +0530

    FIXME initial testing of alternate power stage config

--- rtos/src/main.rs
+++ rtos/src/main.rs
@@ -18,8 +18,8 @@ mod mem;
 assign_resources! {
     usart1: USART1Resource {
         peri: UART8,
-        tx: PJ8,        // UART8 tx
-        rx: PJ9,        // UART8 rx
+        tx: PJ8,        // UART3 tx
+        rx: PJ9,        // UART3 rx
         tx_dma: DMA2_CH0,
         rx_dma: DMA2_CH1,
         rtc_power_key: PG10,
@@ -38,50 +38,31 @@ pub fn init() -> (embassy_stm32::Peripherals, cortex_m::Peripherals) {
     let mut config = embassy_stm32::Config::default();
     {
         use embassy_stm32::rcc::*;
-        // config.rcc.supply_config = SupplyConfig::LDO;
+        config.rcc.supply_config = SupplyConfig::LDO;
         config.rcc.hsi = Some(HSIPrescaler::DIV1); // // 64MHz
         config.rcc.csi = true;
         config.rcc.hsi48 = Some(Hsi48Config {
             sync_from_usb: true,
         }); // needed for USB
 
-        #[cfg(feature = "stm32h747_400")]
-        {
-            config.rcc.pll1 = Some(Pll {
-                source: PllSource::HSI,
-                prediv: PllPreDiv::DIV4,
-                mul: PllMul::MUL50,
-                divp: Some(PllDiv::DIV2), // ((64/4)*50)/2 = 400MHz
-                divq: Some(PllDiv::DIV8), // ((64/4)*50)/8 = 100MHz / SPI1 cksel defaults to pll1_q
-                divr: None,
-            });
-            config.rcc.pll2 = Some(Pll {
-                source: PllSource::HSI,
-                prediv: PllPreDiv::DIV8,
-                mul: PllMul::MUL50,
-                divp: Some(PllDiv::DIV4), // ((64/8)*50)/4 = 100MHz
-                divq: None,
-                divr: None,
-            });
-        }
         #[cfg(feature = "stm32h747_480")]
         {
             config.rcc.pll1 = Some(Pll {
                 source: PllSource::HSI,
-                prediv: PllPreDiv::DIV8,
-                mul: PllMul::MUL120,
+                prediv: PllPreDiv::DIV16,
+                mul: PllMul::MUL240,      // Plln
                 divp: Some(PllDiv::DIV2), // ((64/8)*120)/2 = 480MHz
-                divq: Some(PllDiv::DIV8), // ((64/8)*120)/8 = 120MHz / SPI1 cksel defaults to pll1_q
-                divr: None,
+                divq: Some(PllDiv::DIV2), // ((64/8)*120)/8 = 120MHz / SPI1 cksel defaults to pll1_q
+                divr: Some(PllDiv::DIV2),
             });
-            config.rcc.pll2 = Some(Pll {
-                source: PllSource::HSI,
-                prediv: PllPreDiv::DIV8,
-                mul: PllMul::MUL50,
-                divp: Some(PllDiv::DIV4), // ((64/8)*50)/4 = 100MHz
-                divq: None,
-                divr: None,
-            });
+            // config.rcc.pll2 = Some(Pll {
+            //     source: PllSource::HSI,
+            //     prediv: PllPreDiv::DIV8,
+            //     mul: PllMul::MUL50,
+            //     divp: Some(PllDiv::DIV4), // ((64/8)*50)/4 = 100MHz
+            //     divq: None,
+            //     divr: None,
+            // });
         }
 
         config.rcc.sys = Sysclk::PLL1_P; // 400 Mhz
@@ -91,13 +72,6 @@ pub fn init() -> (embassy_stm32::Peripherals, cortex_m::Peripherals) {
         config.rcc.apb3_pre = APBPrescaler::DIV2; // 100 Mhz
         config.rcc.apb4_pre = APBPrescaler::DIV2; // 100 Mhz
         config.rcc.voltage_scale = VoltageScale::Scale0;
-
-        let mut mux = embassy_stm32::rcc::mux::ClockMux::default();
-        mux.adcsel = embassy_stm32::rcc::mux::Adcsel::PLL2_P;
-        config.rcc.mux = mux;
-
-        // RTC
-        config.rcc.ls = LsConfig::default_lse();
 
         trace!(
             "rcc.voltage_scale = Voltage::Scale{=i32}",
@@ -139,7 +113,7 @@ pub async fn usart_task(r: USART1Resource) {
     ));
 
     for i in 0..2 {
-        unwrap!(usart.blocking_write(b"Hello\r\n"));
+        unwrap!(usart.blocking_write(b"Hello from Rust\r\n"));
         info!("[{}]: wrote Hello, starting echo", &i);
     }
 
